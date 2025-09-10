d6138b452cecb2517a77613bb08d9abce5fd0f33
Joyce Ng <joyce.ng@oocl.com>
2025-09-10 17:10:33 +0800

    refactor: clean up imports and formatting

--- src/main/java/com/example/demo/entity/Company.java
+++ src/main/java/com/example/demo/entity/Company.java
@@ -11,12 +11,15 @@ public class Company {
     public Integer getId() {
         return id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public void setName(String name) {
         this.name = name;
     }
