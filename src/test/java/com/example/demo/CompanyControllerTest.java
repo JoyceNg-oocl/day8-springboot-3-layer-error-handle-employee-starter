830fab537d0f6bfee4f35791ab547efe043b0fc3
Joyce Ng <joyce.ng@oocl.com>
2025-09-10 13:24:07 +0800

    test: enable 404 status check for company retrieval (company)

--- src/test/java/com/example/demo/CompanyControllerTest.java
+++ src/test/java/com/example/demo/CompanyControllerTest.java
@@ -112,15 +112,15 @@ public class CompanyControllerTest {
                 .andExpect(jsonPath("$.length()").value(5));
     }
 
-//    @Test
-//    void should_status_404_when_get_company_by_id_not_found() throws Exception {
-//        MockHttpServletRequestBuilder request = get("/companies/999")
-//                .contentType(MediaType.APPLICATION_JSON);
-//
-//        mockMvc.perform(request)
-//                .andExpect(status().isNotFound());
-//    }
-//
+    @Test
+    void should_status_404_when_get_company_by_id_not_found() throws Exception {
+        MockHttpServletRequestBuilder request = get("/companies/999")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isNotFound());
+    }
+
 //    @Test
 //    void should_status_404_when_put_company_by_id_not_found() throws Exception {
 //        String requestBody = """
