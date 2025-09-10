edd3835422d153294a60b52c78cdfe41fed8c503
Joyce Ng <joyce.ng@oocl.com>
2025-09-10 16:20:14 +0800

    refactor: implement 3 layers for getCompanies (company)

---
+++ src/main/java/com/example/demo/repository/CompanyRepository.java
@@ -0,0 +1,28 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Company;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CompanyRepository {
+    List<Company> companies = new ArrayList<>();
+
+    public void empty() {
+        companies.clear();
+    }
+
+    public List<Company> getCompanies(Integer page, Integer size) {
+        if (page != null && size != null) {
+            int start = (page - 1) * size;
+            int end = Math.min(start + size, companies.size());
+            if (start >= companies.size()) {
+                return new ArrayList<>();
+            }
+            return companies.subList(start, end);
+        }
+        return companies;
+    }
+}
---
+++ src/main/java/com/example/demo/service/CompanyService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Company;
+import com.example.demo.repository.CompanyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public void empty() {
+        companyRepository.empty();
+    }
+
+    public List<Company> getCompanies(Integer page, Integer size) {
+        return companyRepository.getCompanies(page, size);
+    }
+}
